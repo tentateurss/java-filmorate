bf71ebe528e4c4b5acdbe911a9b8b800622e3308
tentateursss <Mativ4ik77@yandex.ru>
2026-03-24 17:44:13 +0300

    Выполенное ТЗ

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.Duration;
 import java.time.LocalDate;
 
 /**
